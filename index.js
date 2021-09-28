3da678f897b8faa80e71afb87d292676543d31e2
evatrel <68126194+evatrel@users.noreply.github.com>
2021-09-28 10:04:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const Discord = require('discord.js');
-const { Routes } = require('discord-api-types/v9');
-const { token } = require('./config.json');
-const fs = require('fs');
 
 const client = new Discord.Client();
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
+client.on('ready', client => {
+	console.log('Ready!');
+});
 
 client.login('ODkyMTgzMDgyNTk2MTcxODQ3.YVJMPg.bhNNIIY8cT5xawPB1tVi3Wdvjyw');
